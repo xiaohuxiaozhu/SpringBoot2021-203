76191ad84b476502dda8e5c88904f6cf82266db1
togoboy8 <cedrichillah@gmail.com>
2019-07-15 13:50:34 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot2021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2021Application.class, args);
+	}
+
+}
